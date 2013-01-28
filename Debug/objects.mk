a1b7b81c813159b2535ea75f7aa1088d1e439b69
Atin M <atinm.dev@gmail.com>
2013-01-27 20:28:43 -0500

    First commit of just commandline

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lxml2 -lgd -lavcodec -lavformat -lxslt -lpq -lswscale
+
---
+++ Debug/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../bdd.cc \
+../commandline.cc \
+../film.cc \
+../graph.cc \
+../image.cc \
+../shot.cc \
+../xml.cc 
+
+OBJS += \
+./bdd.o \
+./commandline.o \
+./film.o \
+./graph.o \
+./image.o \
+./shot.o \
+./xml.o 
+
+CC_DEPS += \
+./bdd.d \
+./commandline.d \
+./film.d \
+./graph.d \
+./image.d \
+./shot.d \
+./xml.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/include/postgresql -I/usr/include/libxml2 -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
